e6b340b153a23cfe110f65b265168b6ea19f3291
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-08-23 15:13:54 -0700

    Added 6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range (0, 10):
+    for j in range(i, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        elif i == j:
+            continue
+        else:
+            print("{}{}".format(i, j), end=", ")
