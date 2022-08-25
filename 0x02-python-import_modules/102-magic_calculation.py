8d792fff1bd44f04702431eed4ddf257834d3d2c
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-08-25 06:14:06 -0700

    Added 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,11 +4,10 @@ def magic_calculation(a, b):
     function performs same as bytecode provided
     """
     from magic_calculation_102 import add, sub
-
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-            return c
+        return (c)
     else:
-        return sub(a, b)
+        return sub(a, b))
