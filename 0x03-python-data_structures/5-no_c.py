b474be38d5f0a5ab5045cf60eaf21e03dac2e5b0
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-08-27 00:15:37 -0700

    fixed 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    """
+    Remove all characters c and C from a string
+    """
     new_string = [l for l in my_string if l != 'c' and l !='C']
-    return ("".join(new_string))
+    return "".join(new_string)
