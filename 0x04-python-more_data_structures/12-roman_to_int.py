5907e76fe85f233088a15b71c2a0549af8ed42d9
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-08-31 08:36:17 -0700

    added 101-square_matrix_map.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,13 +7,10 @@ def roman_to_int(roman_string):
         return 0
     hashMap = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     arr = [hashMap[key] for key in roman_string]
-    if len(arr) == 1:
-        return arr[0]
-    else:
-        sum_ = arr[-1]
-        for idx in range(len(arr)-1, 0, -1):
-            if arr[idx] > arr[idx-1]:
-                sum_ -= arr[idx-1]
-            else:
-                sum_ += arr[idx-1]
-    return sum_
+    num = arr[-1]
+    for idx in range(len(arr)-1, 0, -1):
+        if arr[idx] > arr[idx-1]:
+            num -= arr[idx-1]
+        else:
+            num += arr[idx-1]
+    return num
