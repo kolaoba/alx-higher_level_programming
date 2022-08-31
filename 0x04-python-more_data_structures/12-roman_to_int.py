b873c61d79215b09e92208aee0ec9d981997771b
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-08-31 07:56:19 -0700

    added 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     """
     converts a Roman numeral to an integer.
     """
-    if not roman_string:
+    if not instance(roman_string, str) or not roman_string:
         return 0
     hashMap = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     arr = [hashMap[key] for key in roman_string]
