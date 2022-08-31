f87c30252f158c9bbc3bd9dac7e0bffe300c58aa
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-08-31 09:09:21 -0700

    Added 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,10 +3,10 @@ def complex_delete(a_dictionary, value):
     """
     deletes keys with a specific value in a dictionary.
     """
-    if value not in a_dictionary.values():
-        return a_dictionary
-    new_dict = {}
-    for key in a_dictionary.keys():
-        if a_dictionary[key] != value:
-            new_dict[key] = a_dictionary[key]
-    return new_dict
+    while value in a_dictionary.values():
+        for key, val in a_dictionary.items():
+            if val == value:
+                del a_dictionary[key]
+                break
+
+    return a_dictionary
