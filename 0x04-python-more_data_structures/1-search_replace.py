29631ddf289df5638ceaf98835636e97ca1934b2
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-08-31 05:15:41 -0700

    added 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """
+    replaces all occurences of an element by another in a new list
+    """
+    new_list = []
+    for num in my_list:
+        if num == search:
+            new_list.append(replace)
+        else:
+            new_list.append(num)
+    return new_list
