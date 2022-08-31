ec96ae13b89b6121dd828b9729332442de9ba5f9
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-08-31 06:53:53 -0700

    added 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,5 +4,11 @@ def best_score(a_dictionary):
     returns a key with the biggest integer value.
     """
     if a_dictionary:
-        return max([a_dictionary.get(key, None) for key in a_dictionary.keys()])
+        max_key = None
+        max_value = float('-inf')
+        for key in a_dictionary.keys():
+            if a_dictionary[key] > max_value:
+                max_value = a_dictionary[key]
+                max_key = key
+        return max_key
     return None
