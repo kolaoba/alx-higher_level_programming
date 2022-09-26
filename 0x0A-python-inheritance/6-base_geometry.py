f638c2d5c60659e02d362e378e5b5e736d2e4bcb
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-09-26 10:17:00 -0700

    added 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """defines an empty class BaseGeometry"""
+
+    def area(self):
+        """raises an Exception with the message
+        area() is not implemented
+        """
+
+        raise Exception("area() is not implemented")
