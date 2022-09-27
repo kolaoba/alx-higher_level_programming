f9cc4b97dcee58d986177e50239fda79f5354035
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-09-27 00:15:04 -0700

    Added 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a function that reads a text file (UTF8) and prints to stdout"""
+
+
+def read_file(filename=""):
+    """reads UTF8 file and prints to stdout
+    
+    Args:
+        filename (file): file to be read
+    """
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end='')
