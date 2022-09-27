a8696f15d5e83628649879a0e78c8ca99aea1acb
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-09-27 00:44:15 -0700

    Added 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,5 +5,10 @@ import json
 
 def to_json_string(my_obj):
     """returns the JSON representation of an object(string)
+
+    Args:
+        my_obj (any): object to be converted to JSON
+    Returns:
+        JSON representation of object
     """
     return json.dumps(my_obj)
