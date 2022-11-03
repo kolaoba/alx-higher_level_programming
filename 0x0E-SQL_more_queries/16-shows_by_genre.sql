1c9d148c73049cb5980ed2d5b1450dfbc8e282a2
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-11-03 12:59:27 -0700

    added 16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT
+	ts.title AS title,
+	tg.name AS name
+FROM tv_show AS ts
+LEFT JOIN tv_genres AS tg ON tg.id = tsg.genre_id
+LEFT JOIN tv_show_genres AS tsg ON tsg.show_id = ts.id
+WHERE tg.name = "Comedy"
+ORDER BY ts.title, tg.name;
