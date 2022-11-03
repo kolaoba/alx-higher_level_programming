06e38614427efea0fee7711352e52ff7f23d6016
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-11-03 12:23:54 -0700

    added 13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT
+	tg.name AS genre,
+	COUNT(tg.genre_id) AS number_of_shows
+FROM tv_show_genres AS tsg
+JOIN tv_genres tg ON tg.id = tsg.genre_id
+GROUP BY tsg.genre_id
+ORDER BY number_of_shows DESC, tsg.id ASC;
