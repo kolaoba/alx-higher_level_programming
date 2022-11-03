a74a8933684910416b20ba82d9feabce9b532a9a
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-11-03 13:05:45 -0700

    added 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,5 +5,4 @@ SELECT
 FROM tv_shows AS ts
 LEFT JOIN tv_show_genres AS tsg ON tsg.show_id = ts.id
 LEFT JOIN tv_genres AS tg ON tg.id = tsg.genre_id
-WHERE tg.name = "Comedy"
 ORDER BY ts.title, tg.name;
