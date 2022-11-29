a3f11c37ecfe3802a7ee3744dcdf6d09093e3445
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-11-29 14:07:02 -0800

    Added task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-exports.nbOcurrences = function (list, searchElement) {
+exports.nbOccurrences = function (list, searchElement) {
   return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 };
