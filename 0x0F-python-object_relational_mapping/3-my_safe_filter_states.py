e96e72e11c65f5bd3c6b801db46943360d50e633
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-12-09 13:14:24 -0800

    added task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This script takes in arguments and displays all values in the states table of
+"""
+This script takes in arguments and displays all values in the states table of
 hbtn_0e_0_usa where name matches the argument. But this time, write one that
 is safe from MySQL injections!
 """
