1b674e9190a2bd8b8ff86cf66323adfe31e1a3b4
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-12-09 13:06:13 -0800

    added task 14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Python file that contains the class definition of a City
+"""
+from sqlalchemy import Column, ForeignKey, Integer, String
+from model_state import Base
+
+
+class City(Base):
+    """Represents a city for a MySQL database
+    Attributes:
+        id (str): The city's id
+        name (sqlalchemy.Integer): The city's name
+        state_id (sqlalchemy.String): The city's state id
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
