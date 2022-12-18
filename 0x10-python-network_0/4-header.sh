29aee95f47a06c348bff47d8333be8cc8abab097
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-12-18 02:47:31 -0800

    fixed task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script takes in URL, sends a GET request to the URL
 # with a header variable, and displays the response body
-curl -sH "X-School-USer-Id: 98" "$1"
+curl -sH "X-School-User-Id: 98" "$1"
