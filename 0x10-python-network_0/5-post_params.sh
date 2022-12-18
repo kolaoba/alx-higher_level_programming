648e13795eb69bc788607f4dcc4c6595453bcc48
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-12-18 02:51:05 -0800

    fixed task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script takes in URL, sends a POST request to the URL
-# with a header variable, and displays the response body
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
