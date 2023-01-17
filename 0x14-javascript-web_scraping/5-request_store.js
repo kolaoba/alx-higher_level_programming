bfce5e14e15366c96a7d1970925762744e37dfea
kolaoba <kolaobajuluwa@gmail.com>
2023-01-17 13:40:33 +0100

    reverted ES6 arrow functions

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
-//gets the contents of a webpage 
+// gets the contents of a webpage
 // and stores it in a file
-
 
 const fs = require('fs');
 const request = require('request');
-
 
 const url = process.argv[2];
 const fileName = process.argv[3];
 
 request.get(url,
-		(error, response, body) => {
+  function (error, response, body) {
     if (!error && response.statusCode === 200) {
-        fs.writeFile(fileName, body, (err) => {
-            if (err) throw err;
-            console.log(`Webpage saved to ${fileName}`);
-        });
+      fs.writeFile(fileName, body, function (err) {
+        if (err) console.error(err);
+        console.log(`Webpage saved to ${fileName}`);
+      });
     } else {
-        console.log(`Error: ${error}`);
+      console.log(`Error: ${error}`);
     }
-});
+  });
