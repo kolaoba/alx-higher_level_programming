9c18f369148aa99cf3c6a5d9b31a91be2af0eaf8
kolaoba <kolaobajuluwa@gmail.com>
2023-01-17 13:45:40 +0100

    added documentation, error handling

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
-
+// computes the number of tasks completed by user id
 const request = require('request');
 const url = process.argv[2];
 
 request.get(
-			url,
-			function(err, res, body) {
-				todos = JSON.parse(body);
-				output = {};
-				todos.forEach(function (todo, idx) {
-					if (todo.completed) {
-			if (todo.userId in output) {
-				output[todo.userId]++;
-				} else {
-					output[todo.userId] = 1
-				}
-					}
-				});
-				console.log(output);
-});
+  url,
+  function (err, res, body) {
+    if (err) console.error(err);
+    const todos = JSON.parse(body);
+    const output = {};
+    todos.forEach(function (todo, idx) {
+      if (todo.completed) {
+        if (todo.userId in output) {
+          output[todo.userId]++;
+        } else {
+          output[todo.userId] = 1;
+        }
+      }
+    });
+    console.log(output);
+  });
