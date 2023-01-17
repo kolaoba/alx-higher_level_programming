219e30d03d343f722192085c0d6107f7a91f027c
kolaoba <kolaobajuluwa@gmail.com>
2023-01-17 13:16:38 +0100

    added js tasks

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// writes a string to a file.
+
+const fs = require('fs')
+const file = process.argv[2];
+const input = process.argv[3]
+
+fs.writeFile(file, input, 'utf-8', (err, input) => {
+	if (err) throw err;
+	console.log("added");
+})
+
