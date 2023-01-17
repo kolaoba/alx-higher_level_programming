3f777e78ab7ba4608a5da4db955c91fa9ce232e7
kolaoba <kolaobajuluwa@gmail.com>
2023-01-17 13:23:32 +0100

    replaced throw with console.error

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,6 +6,5 @@ const file = process.argv[2];
 const input = process.argv[3];
 
 fs.writeFile(file, input, 'utf-8', function (err, input) {
-  if (err) throw err;
-  console.log('added');
+  if (err) console.error(err);
 });
