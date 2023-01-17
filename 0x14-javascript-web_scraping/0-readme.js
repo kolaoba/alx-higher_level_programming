f0ab9a22e0324a68ab23cf05c0940b11f7a4f912
kolaoba <kolaobajuluwa@gmail.com>
2023-01-17 13:17:27 +0100

    ran semistandard on 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // reads and prints the content of a file.
 
-const fs = require('fs')
+const fs = require('fs');
 const file = process.argv[2];
 
 fs.readFile(file, 'utf-8', function (err, data) {
-	if (err) throw err;
-	console.log(data);
+  if (err) throw err;
+  console.log(data);
 });
