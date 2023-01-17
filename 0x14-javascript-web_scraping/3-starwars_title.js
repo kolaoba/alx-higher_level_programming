d50f9d708471cdb7e8ad56f16a8075806af4dd8e
kolaoba <kolaobajuluwa@gmail.com>
2023-01-17 13:32:33 +0100

    added error handling for task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
-// 
+// prints the title of a Star Wars movie where
+// the episode number matches a given integer.
 
 const req = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 req.get(url,
-		function (err, res, body) {
-			jsonBody = JSON.parse(body);
-			console.log(jsonBody["title"]);
-		}
+  function (err, res, body) {
+    if (err) console.error(err); else {
+      const jsonBody = JSON.parse(body);
+      console.log(jsonBody.title);
+    }
+  }
 );
